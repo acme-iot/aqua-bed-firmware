42eae80534f5ee9a2fbbaef0a6d18cdea9dd9b3e
trentmillar <trent.millar@gmail.com>
2020-02-22 18:27:04 -0700

    mqtt in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,42 @@
-void setup() {}
-void loop() {}
+/*
+ * Aquaponic planter firmware
+ */
+
+
+#ifdef ESP32
+#include <WiFi.h>
+#else
+#include <ESP8266WiFi.h>
+#endif
+
+
+extern "C" {
+	#include "freertos/FreeRTOS.h"
+	#include "freertos/timers.h"
+}
+
+#include <AsyncMqttClient.h>
+
+
+#define WIFI_SSID "yourSSID"
+#define WIFI_PASSWORD "yourpass"
+
+#define MQTT_HOST IPAddress(192, 168, 1, 10)
+#define MQTT_PORT 1883
+
+AsyncMqttClient mqttClient;
+TimerHandle_t mqttReconnectTimer;
+TimerHandle_t wifiReconnectTimer;
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Initializing...");
+
+}
+
+void loop() {
+  delay(2000);
+  Serial.print(".");
+}
