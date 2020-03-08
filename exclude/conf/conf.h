9e6457750817d717d3cbb667381f38583aad55cd
trentmillar <trent.millar@gmail.com>
2020-03-08 10:44:53 -0600

    moving confg_h

--- exclude/conf/conf.h
+++ exclude/conf/conf.h
@@ -6,10 +6,6 @@
 #include <ArduinoLog.h>
 
 
-//#define config_default_json "{\"unit\":{\"name\":\"SHTech-R\"},\"web\":{\"user\":\"admin\",\"pass\":\"admin\"},\"wifi\":{},\"plugins\":[]}"
-
-//const char * config_filename = "/fs/config.ini";
-
 class Config
 {
     private:
