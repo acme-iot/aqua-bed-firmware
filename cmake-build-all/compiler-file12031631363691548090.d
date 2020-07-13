df07ff67d93bc48fa94c223decf519f30008ce82
trentmillar <trent.millar@gmail.com>
2020-07-13 13:24:24 -0600

    modify ini to externalize port

--- cmake-build-all/compiler-file12031631363691548090.d
+++
@@ -1,2 +0,0 @@
-compiler-file12031631363691548090.o: \
- /private/var/folders/pd/9smsyrwx5wq9zh14bp28w1sr0000gn/T/compiler-file12031631363691548090
--- cmake-build-all/compiler-file8636305171309048768.d
+++
@@ -1,2 +0,0 @@
-compiler-file8636305171309048768.o: \
- /private/var/folders/pd/9smsyrwx5wq9zh14bp28w1sr0000gn/T/compiler-file8636305171309048768
